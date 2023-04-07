2556078615818fd08504df1a875b0c5f13d3a048
Ahmed (Onimisea) Tijani <onimisea@gmail.com>
2023-04-07 23:45:44 +0100

    Update Call2Action.tsx

--- src/components/Call2Action.tsx
+++ src/components/Call2Action.tsx
@@ -6,12 +6,12 @@ type Props = {
 const Call2Action = ({ buttonText, buttonType }: Props) => {
   return (
     <button
-      className={`grid place-items-center ${
+      className={`grid place-items-center w-[100px] h-[30px] ${
         buttonType === "primary"
-          ? "bg-gradient-primsec-f hover:bg-gradient-primsec-b text-white text-sm px-6 py-[5px] rounded-full cursor-pointer duration-500 ml-4 hover:shadow-md hidden sm:flex"
+          ? "bg-gradient-primsec-f hover:bg-gradient-primsec-b text-white text-sm rounded-full cursor-pointer duration-500 ml-4 hover:shadow-md hidden sm:flex"
           : buttonType === "secondary"
-          ? "bg-white text-textColor-light border border-primary-500 hover:bg-gradient-primsec-b hover:border-none hover:text-white text-sm px-6 py-[4px] rounded-full cursor-pointer duration-500 hover:shadow-md hidden sm:flex"
-          : "bg-white hover:bg-gradient-primsec-b text-textColor-light hover:text-white text-sm px-6 py-[5px] rounded-full cursor-pointer duration-500 hover:shadow-md hidden sm:flex"
+          ? "bg-white text-textColor-light border border-primary-500 hover:bg-gradient-primsec-b hover:border-none hover:text-white text-sm rounded-full cursor-pointer duration-500 hover:shadow-md hidden sm:flex"
+          : "bg-white hover:bg-gradient-primsec-b text-textColor-light hover:text-white text-sm rounded-full cursor-pointer duration-500 hover:shadow-md hidden sm:flex"
       }`}
     >
       {buttonText}
