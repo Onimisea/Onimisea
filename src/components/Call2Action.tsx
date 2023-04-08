fd703c9e036aa31af88a7d1b28a183edc26e4310
Ahmed (Onimisea) Tijani <onimisea@gmail.com>
2023-04-08 03:41:05 +0100

    Update Call2Action.tsx

--- src/components/Call2Action.tsx
+++ src/components/Call2Action.tsx
@@ -1,17 +1,19 @@
 type Props = {
   buttonText: string;
   buttonType: string;
+  isMenuToggled: Boolean;
 };
 
-const Call2Action = ({ buttonText, buttonType }: Props) => {
+const Call2Action = ({ buttonText, buttonType, isMenuToggled }: Props) => {
   return (
     <button
       className={`w-fit px-4 pt-2 pb-1  ${
         buttonType === "primary"
-          ? "bg-gradient-primsec-f hover:bg-gradient-primsec-b text-white text-sm rounded-full cursor-pointer duration-500 ml-4 hover:shadow-md hidden lg:flex "
+          ? {`bg-gradient-primsec-f hover:bg-gradient-primsec-b text-white text-sm rounded-full cursor-pointer duration-500 ml-4 hover:shadow-md hidden lg:flex 
+${isMenuToggled ? "flex" : "hidden"}`}
           : buttonType === "secondary"
-          ? "bg-white dark:bg-tertiary-500 text-textColor-light dark:text-gray-200 border dark:border-none border-primary-500 hover:bg-gradient-primsec-b hover:border-none hover:text-white text-sm rounded-full cursor-pointer duration-500 hover:shadow-md hidden lg:flex"
-          : "bg-white dark:bg-tertiary-500 hover:bg-gradient-primsec-b text-textColor-light dark:text-gray-200 hover:text-white text-sm rounded-full cursor-pointer duration-500 hover:shadow-md hidden lg:flex "
+          ? {`bg-white dark:bg-tertiary-500 text-textColor-light dark:text-gray-200 border dark:border-none border-primary-500 hover:bg-gradient-primsec-b hover:border-none hover:text-white text-sm rounded-full cursor-pointer duration-500 hover:shadow-md hidden lg:flex ${isMenuToggled ? "flex" : "hidden"}`}
+          : {`bg-white dark:bg-tertiary-500 hover:bg-gradient-primsec-b text-textColor-light dark:text-gray-200 hover:text-white text-sm rounded-full cursor-pointer duration-500 hover:shadow-md hidden lg:flex ${isMenuToggled ? "flex" : "hidden"}`}
       }`}
     >
       {buttonText}
