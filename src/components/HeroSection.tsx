b6b15c8f1590fb25a072a6899bb078d8e8569fe7
Ahmed (Onimisea) Tijani <onimisea@gmail.com>
2023-04-13 08:33:15 +0100

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -7,7 +7,7 @@ const HeroSection = (props: Props) => {
   return (
     <section className="w-full pt-4 pb-10 md:h-full flex flex-col lg:flex-row items-center justify-center">
       <section className="w-full flex flex-col lg:flex-row items-center justify-center lg:gap-20">
-        <section className="bg-white w-full lg:w-[50%]">
+        <section className="w-full lg:w-[50%]">
           <p className="text-tertiary-500 text-sm">Hello, I&apos;m</p>
 
             <h1 className="text-4xl font-p22Bold">Your Fullstack <span className="text-tertiary-500">Web/3</span> Developer <span className="text-tertiary-500 text-2xl">&</span> <span className="text-3xl">Software Engineer</span></h1>
