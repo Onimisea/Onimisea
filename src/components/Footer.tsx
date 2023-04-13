f1296a1e1e66767aa1ab19d9252b30811f21fdb8
Ahmed (Onimisea) Tijani <onimisea@gmail.com>
2023-04-13 08:40:19 +0100

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,11 @@
 type Props = {}
 const Footer = (props: Props) => {
   return (
-    <footer className="">Footer</footer>
+    <footer className="w-full bg-tertiary-500 py-6">
+<section className="">Logo</section>
+<section className="">Links</section>
+<section className="">Social Icons</section>
+</footer>
   )
 }
 export default Footer
