b2c73f3ea0c77c0ff6b4dcc052a96a1aab6f7e63
Ahmed (Onimisea) Tijani <onimisea@gmail.com>
2023-04-13 07:49:14 +0100

    Create Submit button.tsx

---
+++ src/components/Submit button.tsx
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+
+type Props = {
+  buttonText: string;
+  buttonType: string;
+};
+
+const SubmitButton = ({ buttonText, buttonType }: Props) => {
+  return (
+    <button type="submit"
+      className={`w-fit px-4 pt-2 pb-1  ${
+        buttonType === "primary"
+          ? `bg-gradient-primsec-f hover:bg-gradient-primsec-b text-white text-sm rounded-full cursor-pointer duration-500 ml-4 hover:shadow-md hidden lg:flex`
+          : buttonType === "secondary"
+          ? `bg-white dark:bg-tertiary-500 text-textColor-light dark:text-gray-200 border dark:border-none border-primary-500 hover:bg-gradient-primsec-b hover:border-none hover:text-white text-sm rounded-full cursor-pointer duration-500 hover:shadow-md hidden lg:flex`
+          : `bg-white dark:bg-tertiary-500 hover:bg-gradient-primsec-b text-textColor-light dark:text-gray-200 hover:text-white text-sm rounded-full cursor-pointer duration-500 hover:shadow-md hidden lg:flex`
+      }`}
+    >
+      {buttonText}
+    </button>
+  );
+};
+export default SubmitButton;
