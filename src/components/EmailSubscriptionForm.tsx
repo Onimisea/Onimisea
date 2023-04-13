79473fcf6cd397d3c325f7218ecdffb9c000c2b3
Ahmed (Onimisea) Tijani <onimisea@gmail.com>
2023-04-13 08:17:22 +0100

    Update EmailSubscriptionForm.tsx

--- src/components/EmailSubscriptionForm.tsx
+++ src/components/EmailSubscriptionForm.tsx
@@ -16,11 +16,16 @@ alert(JSON.stringify(data));
 
 
 return (
-<form onSubmit={handleSubmit(onSubmit)} className="w-full bg-gray-300 flex items-start justify-center">
-      <input type="email" {...register("email", { required: "Email Address is required"})} aria-invalid={errors.email ? "true" : "false"} />
-{errors.email && <p role="alert">{errors.email?.message}</p>}
+<form onSubmit={handleSubmit(onSubmit)} className="w-full bg-gray-300 flex flex-col">
 
-      <SubmitButton buttonText="Subscribe" buttonType="primary" />
+<section className="w-full bg-orange-400 flex items-center justify-start">
+      <input type="email" {...register("email", { required: "Email Address is required"})} aria-invalid={errors.email ? "true" : "false"} 
+placeholder="email address" className="bg-white border border-tertiary-500 rounded-full px-4" />
+<SubmitButton buttonText="Subscribe" buttonType="primary" />
+</section>
+
+{errors.email && <p className="text-red-600 w-full" role="alert">{errors.email?.message}</p>}
+    
     </form>
 )
 }
