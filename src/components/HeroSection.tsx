8ca265c741472a0e34a3c61005e6541e6941e728
Ahmed (Onimisea) Tijani <onimisea@gmail.com>
2023-04-13 13:15:15 +0100

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -26,10 +26,10 @@ const HeroSection = (props: Props) => {
         </section>
 
         <section className="bg-gray-100 w-full h-full lg:w-[45%] rounded-md flex flex-col items-center justify-center gap-6 group px-6 py-10">
-<section className="w-[267px] h-[267px] rounded-full bg-gradient-primsec-ir group-hover:bg-gradient-primsec-i
+<section className="w-[250px] h-[250px] rounded-full bg-gradient-primsec-ir group-hover:bg-gradient-primsec-i
  grid place-items-center shadow-lg overflow:hidden">
-<section className="w-[253px] h-[253px] rounded-full bg-white grid place-items-center">
-<Image src={opp} alt="Onimisea | Coolest Dev in Africa" width={250} height={250} loading="eager" className="w-[250px] h-[250px] rounded-full object-cover" />
+<section className="w-[240px] h-[240px] rounded-full bg-white grid place-items-center">
+<Image src={opp} alt="Onimisea | Coolest Dev in Africa" width={237} height={237} loading="eager" className="w-[237px] h-[237px] rounded-full object-cover" />
 </section>
 </section>
 
