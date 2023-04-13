0a7d430e4bb57df2c8a970236cb795ee5f0d7030
Ahmed (Onimisea) Tijani <onimisea@gmail.com>
2023-04-13 13:30:46 +0100

    Update SubmitButton.tsx

--- src/components/SubmitButton.tsx
+++ src/components/SubmitButton.tsx
@@ -10,10 +10,10 @@ const SubmitButton = ({ buttonText, buttonType }: Props) => {
     <button type="submit"
       className={`w-fit px-4 pt-2 pb-1  ${
         buttonType === "primary"
-          ? `bg-gradient-primsec-f hover:bg-gradient-primsec-b text-white text-sm rounded-full cursor-pointer duration-500 ml-4 hover:shadow-md hidden lg:flex`
+          ? `bg-gradient-primsec-f hover:bg-gradient-primsec-b text-white text-sm rounded-full cursor-pointer duration-500 ml-0 lg:ml-4 hover:shadow-md flex`
           : buttonType === "secondary"
-          ? `bg-white dark:bg-tertiary-500 text-textColor-light dark:text-gray-200 border dark:border-none border-primary-500 hover:bg-gradient-primsec-b hover:border-none hover:text-white text-sm rounded-full cursor-pointer duration-500 hover:shadow-md hidden lg:flex`
-          : `bg-white dark:bg-tertiary-500 hover:bg-gradient-primsec-b text-textColor-light dark:text-gray-200 hover:text-white text-sm rounded-full cursor-pointer duration-500 hover:shadow-md hidden lg:flex`
+          ? `bg-white dark:bg-tertiary-500 text-textColor-light dark:text-gray-200 border dark:border-none border-primary-500 hover:bg-gradient-primsec-b hover:border-none hover:text-white text-sm rounded-full cursor-pointer duration-500 hover:shadow-md flex`
+          : `bg-white dark:bg-tertiary-500 hover:bg-gradient-primsec-b text-textColor-light dark:text-gray-200 hover:text-white text-sm rounded-full cursor-pointer duration-500 hover:shadow-md flex`
       }`}
     >
       {buttonText}
