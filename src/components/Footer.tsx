81fe51bd55fa92500da0951590b61e462d84f0ec
Ahmed (Onimisea) Tijani <onimisea@gmail.com>
2023-04-13 10:33:41 +0100

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 type Props = {}
 const Footer = (props: Props) => {
   return (
-    <footer className="w-full bg-primary-500 text-white py-6 flex items-center justify-between">
+    <footer className="w-full bg-primary-500 text-white p-4 flex items-center justify-between">
 <section className="">Logo</section>
 <section className="">Links</section>
 <section className="">Social Icons</section>
