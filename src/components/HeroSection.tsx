9f1f98f520d1760f3eb6245c220203bda3a88cc0
Ahmed (Onimisea) Tijani <onimisea@gmail.com>
2023-04-13 09:49:07 +0100

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -7,7 +7,7 @@ const HeroSection = (props: Props) => {
   return (
     <section className="w-full pt-4 pb-10 md:h-full flex flex-col lg:flex-row items-center justify-center">
       <section className="w-full flex flex-col lg:flex-row items-center justify-between lg:gap-20">
-        <section className="w-full lg:w-[50%]">
+        <section className="w-full lg:w-[45%]">
           <p className="text-tertiary-500 text-md">Hello, I&apos;m</p>
 
             <h1 className="text-2xl md:text-3xl lg:text-4xl font-p22Bold">Your Fullstack <span className="text-tertiary-500">Web/3</span> <br />Developer <span className="text-tertiary-500 text-lg md:text-xl lg:text-2xl">&</span> <span className="text-xl md:text-2xl lg:text-3xl">Software <br />Engineer</span></h1>
@@ -22,7 +22,11 @@ const HeroSection = (props: Props) => {
         </section>
 
         <section className="bg-gray-200 w-full lg:w-[45%] rounded-md flex flex-col items-center justify-center gap-4">
-<section className="w-[250px] h-[250px] rounded-full bg-gradient-primsec-ir hover:bg-gradient-primsec-i">
+<section className="w-[250px] h-[250px] rounded-full bg-gradient-primsec-ir hover:bg-gradient-primsec-i p-2
+ grid place-items-center">
+<section className="w-full h-full rounded-full bg-white font-lmMedium text-2xl grid place-items-center">
+Onimisea
+</section>
 </section>
 
 <section className="w-full flex items-center justify-center spacing-x-4"></section>
