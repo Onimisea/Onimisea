e4e0ddcf6ce3fb211f4bbae60a1a95af41d06a2d
Ahmed (Onimisea) Tijani <onimisea@gmail.com>
2023-09-23 21:32:04 +0100

    Update page.tsx

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,11 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <section className="flex flex-col items-center justify-center bg-gradient-to-tr from-[#007fff] via-white to-[#007fff] min-h-screen">
+    <section className="flex flex-col items-center justify-center bg-gradient-to-tr from-slate-400 via-slate-200 to-slate-400 min-h-screen">
       <main className="">
         <section className="">
         <h1 className="text-4xl font-milkbold bg-gradient-to-r from-[#3219c8] via-[#007fff] to-[#3219c8] text-transparent bg-clip-text">onimisea</h1>
-          <p className="text-md text-[#051550] font-probook mt-2">coolest dev in africa</p>
+          <p className="text-center text-md text-[#051550] font-probook mt-2">coolest dev in africa</p>
         </section>
         
       </main>
