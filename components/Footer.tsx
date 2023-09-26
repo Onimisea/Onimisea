0e9748cf17a46244aa157c6e532b2f956edc813c
Ahmed (Onimisea) Tijani <onimisea@gmail.com>
2023-09-26 04:06:51 +0100

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -18,7 +18,7 @@ const Footer = () => {
   return (
     <footer className="bg-primary dark:bg-background2 text-white z-30 w-full">
       <section className="w-[80%] sm:w-[85%] md:w-[90%] mx-auto max-w-screen-2xl flex items-start justify-center gap-12 md:gap-8 flex-wrap py-16 text-md font-[500]">
-        <section className="w-full md:w-[37%] flex flex-col items-center justify-center gap-6 bg-red-500">
+        <section className="w-full md:w-[37%] flex flex-col items-center justify-center gap-6">
           <Link
             href="/"
             className="flex flex-col items-center justify-center gap-2"
@@ -40,7 +40,7 @@ const Footer = () => {
           </p>
         </section>
 
-        <ul className="w-full sm:w-[35%] md:w-[13%] flex flex-col items-start gap-4 bg-green-500">
+        <ul className="w-full sm:w-[35%] md:w-[13%] flex flex-col items-start gap-4">
           <li className="w-full">
             <Link href="" className="w-full block hover:text-primary2 text-right md:text-left">
               Portfolio
@@ -78,7 +78,7 @@ const Footer = () => {
           </li>
         </ul>
 
-        <ul className="w-full sm:w-[50%] md:w-[30%] flex flex-col items-start gap-4 bg-blue-500">
+        <ul className="w-full sm:w-[50%] md:w-[30%] flex flex-col items-start gap-4">
           <li className="w-full">
             <Link
               href="tel:+2348034023726"
