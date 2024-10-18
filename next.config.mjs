e726fd408cd14de3efce4efe4ee158e461ed9948
Onimisea <onimisea@gmail.com>
2024-10-18 05:43:59 +0100

    Updated nextconfig...

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   distDir: "build",
   output: "standalone",
-  generateBuildId: async () => {
-    return "static-build-id"; // Prevents BUILD_ID file generation
-  },
 };
 
 export default nextConfig;
