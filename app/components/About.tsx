253bfabe26df92074cf60c2ce1ee8549f85264c7
Onimisea <onimisea@gmail.com>
2024-11-08 13:10:58 +0100

    Hide about me

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 export const About = () => {
   return (
     <div>
-      <h1 className="onimisea_text_grd2_outline text-6xl font-milkbold" data-text="About Me">
+      {/* <h1 className="onimisea_text_grd2_outline text-6xl font-milkbold" data-text="About Me">
         About Me
-      </h1>
+      </h1> */}
     </div>
   );
 }
