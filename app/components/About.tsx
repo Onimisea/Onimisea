d8cc11e279dfee9cf8b757cfee633d604f2772d5
Onimisea <onimisea@gmail.com>
2024-11-09 16:58:21 +0100

    Updated about section link...

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -3,7 +3,10 @@ import { Bentogrid } from "./Bentogrid";
 
 export const About = () => {
   return (
-    <div className="flex flex-col items-center justify-center w-full">
+    <div
+      id="about"
+      className="flex flex-col items-center justify-center w-full"
+    >
       <div className="w-[90%] mx-auto relative flex justify-center items-center flex-col py-10 gap-6">
         <Bentogrid />
       </div>
